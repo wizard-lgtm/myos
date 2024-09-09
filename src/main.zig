08fd56d498e6266395de5cfeb042f03d83410ce8
ziggywizard <rippiten23@gmail.com>
2024-09-09 23:56:49 +0300

    multiboot added

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,15 @@
+const Multiboot2Header = @import("./multiboot.zig").Multiboot2Header;
+
+export var multiboot align(4) linksection(".multiboot") = Multiboot2Header{
+    .magic = 0xE85250D6, // magic number
+    .architecture = 0, // riscv
+    .header_length = 24, // length
+    .checksum = @intCast(0xFFFFFFFF - (0xE85250D6 + 0 + 24) + 1), // correct wrapping for u32
+
+};
+
 // start
-export fn _start() void {
+export fn _start() noreturn {
     // loop
     while (true) {}
 }
--- src/multiboot.zig
+++ src/multiboot.zig
@@ -1,13 +1,6 @@
-pub packed struct Multiboot2Header {
- magic: u32,
- architecture: u32,
- header_length: u32,
- checksum: u32,
-};
-
-pub const multiboot2: MultiBootHeader {
-  .magic = 0xE85250D6,
-  .architecture: 0,
-  .header_length: 24,
-  checksum: -(0xE85250D6 + 0 + 24)
+pub const Multiboot2Header = packed struct {
+    magic: u32,
+    architecture: u32,
+    header_length: u32,
+    checksum: u32,
 };
