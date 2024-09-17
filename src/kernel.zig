782b1592177486ab7b36764b81f318506c468cfc
Bekir Tolga Karaca <Bekir2.Karaca@cizmeci.local>
2024-09-17 06:42:27 +0300

    mstatus asm func added | interrupt basics

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,10 +1,29 @@
 const uart = @import("./drivers/uart.zig");
+pub fn cssr_mstatus() void {
+    asm volatile ("csrr t0, mstatus");
+}
+const std = @import("std");
+fn u64_to_str(n: u64) []u8 {
+    var buffer: [20]u8 = undefined;
+    const result = std.fmt.bufPrint(&buffer, "{d}", .{n});
+
+    if (result) |value| {
+        return buffer[0..value.len];
+    } else |_| {
+        return &[_]u8{}; // Return an empty slice in case of error
+    }
+    return buffer;
+}
+
 // kernel.zig
 export fn kmain() void {
-    uart.print("Hello world!\n");
+    _ = cssr_mstatus();
 
-    uart.weird('a');
-    while (true) {
-        uart.init();
+    for (0..5) |i| {
+        uart.print("Hello world");
+        uart.print(u64_to_str(i));
+        uart.print("\n");
     }
+
+    while (true) {}
 }
