67e2065068ded6ca8266cd1bf47ec6159377a10a
wizard-lgtm <rippiten23@gmail.com>
2024-09-20 03:29:06 +0000

    set trap handler added

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,29 +1,13 @@
-const uart = @import("./drivers/uart.zig");
-pub fn cssr_mstatus() void {
-    asm volatile ("csrr t0, mstatus");
-}
 const std = @import("std");
-fn u64_to_str(n: u64) []u8 {
-    var buffer: [20]u8 = undefined;
-    const result = std.fmt.bufPrint(&buffer, "{d}", .{n});
-
-    if (result) |value| {
-        return buffer[0..value.len];
-    } else |_| {
-        return &[_]u8{}; // Return an empty slice in case of error
-    }
-    return buffer;
-}
-
+const uart = @import("./drivers/uart.zig");
+const _asm = @import("./asm.zig");
+const interrupts = @import("./interrupts.zig");
 // kernel.zig
 export fn kmain() void {
-    _ = cssr_mstatus();
+    interrupts.set_trap_handler(interrupts.trap_handler);
 
-    for (0..5) |i| {
-        uart.print("Hello world");
-        uart.print(u64_to_str(i));
-        uart.print("\n");
-    }
+    uart.init();
+    uart.print("Hello from myos");
 
     while (true) {}
 }
