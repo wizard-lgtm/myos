eaac2eaa9a6e496ae3fab241f583441e223b369c
wizard-lgtm <rippiten23@gmail.com>
2024-09-23 05:26:18 +0300

    interrupts

--- src/kernel.zig
+++ src/kernel.zig
@@ -1,13 +1,26 @@
 const std = @import("std");
-const uart = @import("./drivers/uart.zig");
+const Uart = @import("./drivers/uart.zig").Uart;
 const _asm = @import("./asm.zig");
 const interrupts = @import("./interrupts.zig");
 // kernel.zig
 export fn kmain() void {
-    interrupts.set_trap_handler(interrupts.trap_handler);
+    var sstatus: usize = _asm.sstatus_read();
 
-    uart.init();
-    uart.print("Hello from myos");
+    const str = "Hello";
+    var uart = Uart{};
+
+    uart.printf("format format {s}: {d}\n", .{ str, 19992 });
+    uart.printf("status {b}\n", .{sstatus});
+
+    // enable supervisor level interrupts
+    interrupts.enable_supervisor_interrupts();
+    uart.printf("interrupts enabled!\n", .{});
+
+    sstatus = _asm.sstatus_read();
+
+    uart.printf("status {b}\n", .{sstatus});
+
+    interrupts.set_trap_handler(interrupts.trap_handler);
 
     while (true) {}
 }
