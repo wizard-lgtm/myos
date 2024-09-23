bfd8e252523675ab60d730ffb9adb7250b63f010
wizard-lgtm <rippiten23@gmail.com>
2024-09-23 05:25:43 +0300

    qemu bin added

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-qemu-system-riscv64 -machine virt -kernel zig-out/bin/myos.elf -serial mon:stdio -nographic
+qemu-system-riscv64 -machine virt -bios fw_dynamic.bin -kernel zig-out/bin/myos.elf -serial mon:stdio -nographic
