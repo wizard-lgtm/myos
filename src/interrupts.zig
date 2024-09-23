b8030ce10810f07ddba4ee4283a64abc1e4d8356
wizard-lgtm <rippiten23@gmail.com>
2024-09-23 05:25:36 +0300

    interrupt table set functions added

--- src/interrupts.zig
+++ src/interrupts.zig
@@ -1,26 +1,43 @@
-const uart = @import("./drivers/uart.zig");
+const Uart = @import("./drivers/uart.zig").Uart;
+const _asm = @import("./asm.zig");
 
 pub fn set_trap_handler(trap_handler_ptr: *const fn () void) void {
-    asm volatile ("csrw mtvec, %[trap_handler_ptr]"
-        :
-        : [trap_handler_ptr] "r" (trap_handler_ptr),
-    );
+    var uart = Uart{};
+
+    const STVEC_MODE: usize = 0b00; // zero for no mapping, single function
+
+    const trap_handler_addr: usize = @intFromPtr(trap_handler_ptr);
+
+    const SET_ZERO: usize = 0b11;
+
+    const stvec_addr_space: usize = trap_handler_addr & ~SET_ZERO; // ~ makes first 2 bit setted to 0 and the other 2..64 is the address;
+
+    const stvec_value: usize = stvec_addr_space | STVEC_MODE;
+
+    // read stvec after write for debugging
+    var stvec: usize = _asm.stvec_read();
+
+    uart.printf("stvec: {b}\n", .{stvec});
+
+    _asm.stvec_write(stvec_value);
+
+    stvec = _asm.stvec_read();
+
+    uart.printf("stvec: {b}\n", .{stvec});
+
+    uart.printf("trap handler addr is setted to {p}\n", .{trap_handler_ptr});
     return;
 }
 
+pub fn enable_supervisor_interrupts() void {
+    var sstatus = _asm.sstatus_read();
+    const SIE_BIT: u64 = 1 << 1;
+    sstatus |= SIE_BIT;
+    _asm.sstatus_write(sstatus);
+}
+
 pub fn trap_handler() void {
-    uart.print("Trap occured");
-    uart.print("Unimplemented!");
-}
-pub fn exception_handler() void {
-    uart.print("Exception occured");
-    uart.print("Unimplemented!");
-}
-pub fn interrupt_handler() void {
-    uart.print("Interrupt occured");
-    uart.print("Unimplemented!");
-}
-pub fn panic_handler() void {
-    uart.print("Kernel panic occured");
-    uart.print("Unimplemented!");
+    var uart = Uart{};
+    uart.printf("Trap occured\n", .{});
+    uart.printf("Unimplemented\n", .{});
 }
