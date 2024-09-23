c80aec8161be3d0bf4c4d2eaebc6a95b82d692aa
wizard-lgtm <rippiten23@gmail.com>
2024-09-23 05:25:59 +0300

    sstatus read write stvec read write functions added

--- src/asm.zig
+++ src/asm.zig
@@ -1,9 +1,26 @@
-pub fn mstatus_read() usize {
+pub fn sstatus_read() usize {
     var result: usize = 0;
-    asm volatile ("csrr %[result], mstatus"
+    asm volatile ("csrr %[result], sstatus"
         : [result] "=r" (result),
     );
     return result;
 }
-pub fn mcause_read() void {}
-pub fn mtvec_read() void {}
+pub fn sstatus_write(value: usize) void {
+    asm volatile ("csrw sstatus, %[value]"
+        :
+        : [value] "r" (value),
+    );
+}
+pub fn stvec_write(value: usize) void {
+    asm volatile ("csrw stvec, %[value]"
+        :
+        : [value] "r" (value),
+    );
+}
+pub fn stvec_read() usize {
+    var value: usize = 0;
+    asm volatile ("csrr %[value], stvec"
+        : [value] "=r" (value),
+    );
+    return value;
+}
