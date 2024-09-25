739e040a4fd352b706bc68bda3536ae436951b04
wizard-lgtm <rippiten23@gmail.com>
2024-09-25 06:06:54 +0300

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-qemu-system-riscv64 -machine virt -bios fw_dynamic.bin -kernel zig-out/bin/myos.elf -serial mon:stdio -nographic
+qemu-system-riscv64 \
+-machine virt \
+-bios fw_dynamic.bin \
+-kernel zig-out/bin/myos.elf \
+-serial mon:stdio \
+-nographic \
+-d int \
+-device virtio-keyboard-pci
